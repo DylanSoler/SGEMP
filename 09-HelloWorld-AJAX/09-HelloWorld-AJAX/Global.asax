947366c22f674db1b92226d49630a4e009951c10
Unknown <dylanadriansoler@gmail.com>
2018-12-04 20:55:14 +0100

    HelloWorld llamada AJAX javascript

---
+++ 09-HelloWorld-AJAX/09-HelloWorld-AJAX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_09_HelloWorld_AJAX.MvcApplication" Language="C#" %>
---
+++ 09-HelloWorld-AJAX/09-HelloWorld-AJAX/Scripts/FuncionesAJAX.js
@@ -0,0 +1,35 @@
+﻿
+window.onload = inicializaEventos;
+
+function inicializaEventos(){
+
+    document.getElementById("btnHello").addEventListener("click", llamada, false);
+
+}
+
+function llamada() {
+
+    //alert('Hola Mundo');
+    
+    var miLlamada = new XMLHttpRequest();
+    //miLlamada.open("GET", "/Home/Index");
+    miLlamada.open("GET", "https://apirestpersonasdylan.azurewebsites.net/api/Personas/4");
+
+    //mientras vienen los datos
+    miLlamada.onreadystatechange = function () {
+
+        alert(miLlamada.readyState);
+
+        if (miLlamada.readyState < 4)
+        {
+            document.getElementById("textoMostrar").innerHTML = "Cagrando.....";
+        }
+        else if (miLlamada.readyState == 4 && miLlamada.status == 200)
+        {
+            document.getElementById("textoMostrar").innerHTML = miLlamada.responseText;
+        }
+    }
+
+    miLlamada.send();
+
+}
