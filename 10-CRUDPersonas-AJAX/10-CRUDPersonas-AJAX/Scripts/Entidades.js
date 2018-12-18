a11ec6e73309abdae0dd113098bb8c25dcf4ed13
Unknown <dylanadriansoler@gmail.com>
2018-12-18 22:23:27 +0100

    en proceso

--- 10-CRUDPersonas-AJAX/10-CRUDPersonas-AJAX/Scripts/Entidades.js
+++ 10-CRUDPersonas-AJAX/10-CRUDPersonas-AJAX/Scripts/Entidades.js
@@ -1,1 +1,10 @@
-﻿
+﻿class Persona {
+    constructor(nombre, apellidos, fechaNacimiento, direccion, telefono, idDepartamento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.idDepartamento = idDepartamento;
+    }
+}
--- 10-CRUDPersonas-AJAX/10-CRUDPersonas-AJAX/Scripts/FuncionesAJAX.js
+++ 10-CRUDPersonas-AJAX/10-CRUDPersonas-AJAX/Scripts/FuncionesAJAX.js
@@ -3,8 +3,11 @@
 function inicializaEventos() {
 
     cargarLista();
+    document.getElementById("create").addEventListener("click", clickCrear, false);
 }
 
+
+// Hace una peticion get a la api y ejecuta la funcion crearTabla a partir del resultado obtenido
 function cargarLista() {
 
     var miLlamada = new XMLHttpRequest();
@@ -12,32 +15,32 @@ function cargarLista() {
 
     miLlamada.onreadystatechange = function () {
 
-        alert(miLlamada.readyState);
-
-        if (miLlamada.readyState < 4) {
-            //document.getElementById("textoMostrar").innerHTML = "Cagrando.....";
-        }
-        else if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
 
             var arrayPersonas = JSON.parse(miLlamada.responseText);
             crearTabla(arrayPersonas);
-
         }
     }
-    miLlamada.send();
 
+    miLlamada.send();
 }
 
+
+//Crea una tabla de personas html a partir de un listado
 function crearTabla(listado) {
 
+    //Crear tabla
     var table = document.getElementById("tablaPersonas");
 
-    for (var i = 0; i < listado.length; i++)
-    {
+
+    //insertar filas
+    for (var i = 0; i < listado.length; i++) {
         var fila = document.createElement("tr");
 
-        for (var prop in listado[0])
-        {
+        //  insertar celdas por fila
+
+        //insertar celdas de las propiedades
+        for (var prop in listado[0]) {
             var celda = document.createElement("td");
             var textCelda = document.createTextNode(listado[i][prop]);
 
@@ -45,25 +48,133 @@ function crearTabla(listado) {
             fila.appendChild(celda);
         }
 
-        //boton BUENOOOOOOOOOOO
+        //insertar celda de botones
         var celdaBtn = document.createElement("td");
-        var btnEditar = document.createElement("button");
-        btnEditar.type = "button";
-        btnEditar.setAttribute("id", listado[i].id);
-        //var image = document.createElement("img");
-        //image.setAttribute("src", "edit.png");
-        //btnEditar.appendChild(image);
-        btnEditar.addEventListener("click", function () {clickEditar}, false);                          
-        celdaBtn.appendChild(btnEditar);
+        //boton editar
+        var editar = document.createElement("input");
+        editar.setAttribute("type", "image");
+        editar.setAttribute("id", listado[i].idPersona);
+        editar.setAttribute("src", "Imagenes/edit.png");
+        editar.setAttribute("width", "30");
+        editar.setAttribute("heigth", "30");
+        editar.addEventListener("click", clickEditar, false);
+        //boton eliminar
+        var eliminar = document.createElement("input");
+        eliminar.setAttribute("type", "image");
+        eliminar.setAttribute("id", listado[i].idPersona);
+        eliminar.setAttribute("src", "Imagenes/delete.png");
+        eliminar.setAttribute("width", "30");
+        eliminar.setAttribute("heigth", "30");
+        eliminar.addEventListener("click", clickEliminar, false);
 
+        //insertar en la celda los botones
+        celdaBtn.appendChild(editar);
+        celdaBtn.appendChild(eliminar);
+        //insertar celda en la fila
         fila.appendChild(celdaBtn);
 
+        //insertar fila a la tabla
         table.appendChild(fila);
     }
+}
 
-    function clickEditar() {
+//Editar persona
+function clickEditar() {
 
-        alert('funca');
+        
+}
+
+//Eliminar persona
+function clickEliminar() {
+
+    var elim = modalEliminar();
+
+    if (elim) {
+        var id = this.id;
+        var llamadaEliminar = new XMLHttpRequest();
+        var ruta = "https://apirestpersonasdylan.azurewebsites.net/api/Personas/" + id;
+        llamadaEliminar.open("DELETE", ruta);
+
+        llamadaEliminar.onreadystatechange = function () {
+
+            if (llamadaEliminar.readyState == 4 && llamadaEliminar.status == 200) {
+
+                alert("Persona borrada con exito!");
+                actualizarTabla();
+            }
+        }
+        llamadaEliminar.send();
+    }
+}
+
+//Crear persona
+function clickCrear() {
+
+    var crear = document.getElementById("modalCrear");
+    var btnCrear = document.getElementById("botonCrear");
+    var btnCancelar = document.getElementById("botonCancelar");
+    crear.style.display = "block";
+
+    btnCrear.onclick = function () {
+
+        var nom = document.getElementById("nombre").value;
+        var ape = document.getElementById("apellidos").value;
+        var fec = document.getElementById("fechaNacimiento").value;
+        var dir = document.getElementById("direccion").value;
+        var tfn = document.getElementById("telefono").value;
+        var idD = document.getElementById("idDepartamento").value;
+
+        var oP = new Persona(nom, ape, fec, dir, tfn, idD);
+        alert("Nombre: " + oP.nombre);
+
+        crear.style.display = "none";
+    }
+
+    btnCancelar.onclick = function () {
+        crear.style.display = "none";
     }
 
 }
+
+//Actualizar tabla
+function actualizarTabla() {
+
+    var table = document.getElementById("tablaPersonas");
+    var rowCount = table.rows.length;
+    for (var i = 1; i < rowCount; i++) {
+        table.deleteRow(1);
+    }
+    cargarLista();
+}
+
+//Modal eliminar
+function modalEliminar() {
+
+    var ret = false;
+    var modal = document.getElementById("modalEliminar");
+    var aceptar = document.getElementById("AceptarEliminar");
+    var cancelar = document.getElementById("CancelarEliminar");
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    aceptar.onclick = function () {
+        modal.style.display = "none";
+        ret = true;
+    }
+
+    cancelar.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    //window.onclick = function (event) {
+    //    if (event.target == modal) {
+    //        modal.style.display = "none";
+    //    }
+    //}
+
+
+
+    return ret;
+}
+
